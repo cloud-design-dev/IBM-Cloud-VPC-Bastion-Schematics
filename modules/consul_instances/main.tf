a77b095bf681b80a7d06f0fe9230c24878be6ef3
greyhoundforty <ryantiffany@fastmail.com>
2020-06-11 13:29:16 -0500

    Added in consul instances in second zone
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- modules/consul_instances/main.tf
+++ modules/consul_instances/main.tf
@@ -13,7 +13,7 @@ resource "ibm_is_instance" "consul" {
   tags           = ["ryantiffany", var.vpc_name]
 
   vpc       = var.vpc_id
-  zone      = "${var.region}-1"
+  zone      = "${var.region}-2"
   keys      = [data.ibm_is_ssh_key.ssh_key.id]
   user_data = file("${path.module}/consul_install.sh")
 }
