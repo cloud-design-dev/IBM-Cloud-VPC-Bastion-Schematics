98f87b985de77f07a9e4042ee5a729e0e5bcfe70
greyhoundforty <ryantiffany@fastmail.com>
2020-06-15 07:29:31 -0500

    Fixed issue with lb pool lb id
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- modules/zones/zone1/loadbalancer.tf
+++ modules/zones/zone1/loadbalancer.tf
@@ -14,7 +14,7 @@ resource "ibm_is_lb_listener" "private_instance_listener" {
 }
 
 resource "ibm_is_lb_pool" "private_instance_pool" {
-  lb                 = ibm_is_subnet.private_subnet.id
+  lb                 = ibm_is_lb.private_instance_lb.id
   name               = "${var.vpc_name}-private-instance-pool"
   protocol           = "http"
   algorithm          = "round_robin"
@@ -28,7 +28,7 @@ resource "ibm_is_lb_pool" "private_instance_pool" {
 
 resource "ibm_is_lb_pool_member" "private_instance_pool_member_zone" {
   count          = var.instance_count
-  lb             = ibm_is_subnet.private_subnet.id
+  lb             = ibm_is_lb.private_instance_lb.id
   pool           = ibm_is_lb_pool.private_instance_pool.id
   port           = "80"
   target_address = element(ibm_is_instance.private_instance[*].primary_network_interface[0].primary_ipv4_address, count.index)
@@ -36,7 +36,7 @@ resource "ibm_is_lb_pool_member" "private_instance_pool_member_zone" {
 }
 
 resource "ibm_is_lb_listener_policy" "lb_listener_policy" {
-  lb        = ibm_is_subnet.private_subnet.id
+  lb        = ibm_is_lb.private_instance_lb.id
   listener  = ibm_is_lb_listener.private_instance_listener.listener_id
   action    = "forward"
   priority  = 1
@@ -45,7 +45,7 @@ resource "ibm_is_lb_listener_policy" "lb_listener_policy" {
 }
 
 resource "ibm_is_lb_listener_policy_rule" "lb_listener_policy_rule" {
-  lb        = ibm_is_subnet.private_subnet.id
+  lb        = ibm_is_lb.private_instance_lb.id
   listener  = ibm_is_lb_listener.private_instance_listener.listener_id
   policy    = ibm_is_lb_listener_policy.lb_listener_policy.policy_id
   condition = "equals"
