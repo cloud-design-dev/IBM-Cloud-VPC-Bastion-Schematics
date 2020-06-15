aa98bd88ba8ca49d91dfc596716ae36bca273f27
greyhoundforty <ryantiffany@fastmail.com>
2020-06-15 11:00:22 -0500

    Adding seperate IPs to output
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- output.tf
+++ output.tf
@@ -14,3 +14,15 @@ output "vpc_name" {
 output "web_instance_ips" {
   value = module.web_instances.instance_ip
 }
+
+output "web_1_ip" {
+  value = module.web_instances.instance_ip[0]
+}
+
+output "web_2_ip" {
+  value = module.web_instances.instance_ip[1]
+}
+
+output "web_3_ip" {
+  value = module.web_instances.instance_ip[2]
+}
